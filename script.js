7366521da5400fe919f2a2d5ecfbc3af54969818
younusaliakash <younusalikash8@gmail.com>
2023-06-05 00:11:51 +0600

    fixed

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
-const start_btn = document.getElementById('start-btn');
-const game_container = document.getElementById('game-container');
-const timeEl = document.getElementById('time');
-const scoreEl = document.getElementById('score');
-const message = document.getElementById('message');
-
-let score = 0;
-let seconds = 0;
+const start_btn = document.getElementById('start-btn')
+const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
 let selected_insect = {}
 
-start_btn.addEventListener('click', () => screens[0].classList.add('up'));
+start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 
 choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        console.log('click')
         const img = btn.querySelector('img')
         const src = img.getAttribute('src')
         const alt = img.getAttribute('alt')
@@ -32,10 +30,8 @@ function startGame() {
 function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
-
     m = m < 10 ? `0${m}` : m
     s = s < 10 ? `0${s}` : s
-
     timeEl.innerHTML = `Time: ${m}:${s}`
     seconds++
 }
@@ -46,24 +42,18 @@ function createInsect() {
     const { x, y } = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
-
     insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style="transform: rotate(${Math.random() * 360}deg)" />`
 
     insect.addEventListener('click', catchInsect)
 
     game_container.appendChild(insect)
-
 }
-
-
 
 function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
-
     const x = Math.random() * (width - 200) + 100
     const y = Math.random() * (height - 200) + 100
-
     return { x, y }
 }
 
@@ -74,18 +64,15 @@ function catchInsect() {
     addInsects()
 }
 
-function increaseScore() {
-    score++
-    if (score > 19) {
-        message.classList.add('visible')
-    }
-
-    scoreEl.innerHTML = `Score: ${score}`
-}
-
-
 function addInsects() {
     setTimeout(createInsect, 1000)
     setTimeout(createInsect, 1500)
 }
 
+function increaseScore() {
+    score++
+    if(score > 19) {
+        message.classList.add('visible')
+    }
+    scoreEl.innerHTML = `Score: ${score}`
+}
